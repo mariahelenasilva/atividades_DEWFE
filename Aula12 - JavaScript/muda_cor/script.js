b99e7ab87310e7ba3aec4db6bc3d7ed7508c40a3
Maria Helena <maria.h.silva42@aluno.senai.br>
2026-05-20 22:24:48 -0300

    adicao aula 12

---
+++ Aula12 - JavaScript/muda_cor/script.js
@@ -0,0 +1,22 @@
+/*
+*@document script.js
+*@name Maria Helena
+*@date 20/05/2026
+*@description Script documento da aula 12 - JavaScript - Muda cor de fundo 
+*/
+
+
+function corVermelha() {
+    document.body.style.backgroundColor = 'red';
+}
+function corVerde() {
+    document.body.style.backgroundColor = 'green';
+}
+function corAzul() {
+    document.body.style.backgroundColor = 'blue';
+}
+
+//função altera cor de fundo da pagina
+function mudaCor(cor) {
+    document.body.style.backgroundColor = cor;
+}
